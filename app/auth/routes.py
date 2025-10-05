54fb522390a18c9be40165c4e026ec2a637ec02b
tihomirtx88 <tihomirtx88@gmail.com>
2025-10-05 12:42:52 +0300

    Implement register functionality

---
+++ app/auth/routes.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, login_required, current_user
+from app import db
+from app.models import User
+
+auth = Blueprint("auth", __name__, template_folder="templates")
+
+@auth.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        existing_user = User.query.filter_by(email=email).first()
+
+        if existing_user:
+            flash("Email already registered!", "warning")
+            return redirect(url_for("auth.register"))
+
+        new_user = User(username=username, email=email)
+        new_user.set_password(password)
+        db.session.add(new_user)
+        db.session.commit()
+
+
+        flash("Registration successful! Please log in.", "success")
+        return redirect(url_for("auth.login"))
+    
+    return render_template("register.html")
+
+
+
