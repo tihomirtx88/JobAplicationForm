4edaba5f99faa39230c8372fb16116eebe4f7ca9
tihomirtx88 <tihomirtx88@gmail.com>
2025-10-05 12:32:56 +0300

    Update model and ad user model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,10 @@
 from . import db
 from datetime import datetime
 
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db, login_manager
+
 class Form(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80))
@@ -24,3 +28,23 @@ class Form(db.Model):
             "date": self.date.isoformat(),
             "occupation": self.occupation,
         }
+
+# Login
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
+
+    def set_password(self, password):
+        """Hash the password before storing."""
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        """Verify a given password."""
+        return check_password_hash(self.password_hash, password)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
